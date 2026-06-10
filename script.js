710e8dcc246a4d940eded160db026d3310b08ef7
1914_gaming_yt <an493369@gmail.com>
2026-06-10 19:17:04 +0430

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    color: { value: "#00ffcc" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    line_linked: {
+      enable: true,
+      color: "#00ffcc"
+    },
+    move: {
+      enable: true,
+      speed: 2
+    }
+  }
+});
